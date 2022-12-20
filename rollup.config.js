9b3988fd249ab98a423aa88a1f1edad7a6c7a6c6
Miquel Rossello <mkpuig@gmail.com>
2022-12-20 14:02:54 +0100

    revert package

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,39 @@
-import commonjs from '@rollup/plugin-commonjs'
-import { nodeResolve } from '@rollup/plugin-node-resolve'
-import { terser } from 'rollup-plugin-terser'
-import globals from 'rollup-plugin-node-globals'
-import json from '@rollup/plugin-json'
-import builtins from 'rollup-plugin-node-builtins'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import globals from "rollup-plugin-node-globals";
+import json from "@rollup/plugin-json";
+import builtins from "rollup-plugin-node-builtins";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const config = {
-  input: 'index.js',
+  input: "index.js",
   output: [
+    //  {
+    //    globals: {
+    //      bsv: "bsv",
+    //    },
+    //    format: "umd",
+    //    name: "stas",
+    //    file: "dist/stas-umd.js",
+    //  },
+    //  {
+    //    globals: {
+    //      bsv: "bsv",
+    //    },
+    //    dir: "./dist",
+    //    format: "esm",
+    //    sourcemap: true,
+    //  },
     {
       globals: {
-        bsv: 'bsv'
+        bsv: "bsv",
       },
-      format: 'umd',
-      name: 'stas',
-      file: 'dist/stas-umd.js'
+      format: "iife",
+      name: "stas",
+      file: "dist/stas-iife.js",
+      //sourcemap: "inline",
     },
-    {
-      globals: {
-        bsv: 'bsv'
-      },
-      dir: './dist',
-      format: 'esm',
-      sourcemap: true
-    }
   ],
   plugins: [
     commonjs(),
@@ -33,8 +42,8 @@ const config = {
     builtins(),
     nodeResolve(),
     terser(),
-    json()
-  ]
-}
+    json(),
+  ],
+};
 
-export default config
+export default config;
